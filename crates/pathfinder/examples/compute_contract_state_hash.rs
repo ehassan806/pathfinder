4aab453d0619577ccc659bb5eb8c144f6a9adb79
Eh2229 <150701701+ehassan806@users.noreply.github.com>
2023-11-18 11:40:37 -0500

    Update compute_contract_state_hash.rs

--- crates/pathfinder/examples/compute_contract_state_hash.rs
+++ crates/pathfinder/examples/compute_contract_state_hash.rs
@@ -2,50 +2,38 @@ use anyhow::Context;
 use pathfinder_crypto::{hash::pedersen_hash, Felt};
 
 fn main() -> anyhow::Result<()> {
-    let mut args = std::env::args();
-    let myself = args
-        .next()
-        .unwrap_or_else(|| String::from("compute_contract_state_hash"));
+    let mut args = std::env::args().skip(1); // Skip the first argument, which is the program name
 
-    let args = args
-        .map(|x| Felt::from_hex_str(&x).map(Some))
-        .chain(std::iter::repeat_with(|| Ok(None)));
+    let myself = args.next().unwrap_or_else(|| String::from("compute_contract_state_hash"));
+
+    let args = args.map(|x| Felt::from_hex_str(&x).map(Some)).chain(std::iter::repeat_with(|| Ok(None)));
 
     let mut description = String::with_capacity(1 + 64 + 64 + 64 + 64 + 4);
     description.push('#');
 
     let res = args
         .enumerate()
-        // was thinking this would be like a column value so start from 1
-        .map(|(nth, x)| (nth + 1, x))
-        // build the description up with an inspect
         .inspect(|(_, x)| {
-            use std::fmt::Write;
             if let Ok(x) = x {
-                write!(description, " {:x}", x.unwrap_or(Felt::ZERO)).unwrap();
+                description.push_str(&format!(" {:x}", x.unwrap_or(Felt::ZERO)));
             }
         })
         .take(4)
-        .try_fold(None, |acc, next| {
-            let nth = next.0 + 1;
-            let next = next
-                .1
-                .with_context(|| format!("Failed to parse {nth} parameter"))?;
+        .try_fold(None, |acc, (nth, x)| {
+            let nth = nth + 1;
+            let next = x.with_context(|| format!("Failed to parse {} parameter", nth))?;
             let next = if nth < 2 {
-                next.with_context(|| format!("Missing {nth} parameter"))?
+                next.with_context(|| format!("Missing {} parameter", nth))?
             } else {
                 next.unwrap_or(Felt::ZERO)
             };
-            Ok::<_, anyhow::Error>(acc.map(|prev| pedersen_hash(prev, next)).or(Some(next)))
+            Ok(acc.map(|prev| pedersen_hash(prev, next)).or(Some(next)))
         })
-        .with_context(|| {
-            format!("USAGE: {myself} class_hash tree_root [nonce [contract_version]]")
-        })?
+        .with_context(|| format!("USAGE: {} class_hash tree_root [nonce [contract_version]]", myself))?
         .expect("there is always iterated over value");
 
-    println!("{description}");
-
-    println!("{res:x}");
+    println!("{}", description);
+    println!("{:x}", res);
 
     Ok(())
 }
